ac5be1c00afdbe96c942e2e99e4e3a6f8804f0f6
WindaSitorus <windasitorus0197@gmail.com>
2024-09-27 10:39:24 +0700

    pair13

--- T02.java
+++ T02.java
@@ -1,1 +1,45 @@
-// NIM - Your Name
+// 12S24001-Fredrick Aritonang
+// 12S24019-Winda N.V. Sitorus
+import java.util.*;
+import java.lang.Math;
+
+public class T02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String isbn, judul, penulis, penerbit, formatbuku, kategori;
+        int tahunterbit, stok;
+        double hargapembelian, minimummargin, rating;
+
+        isbn = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        penerbit = input.nextLine();
+        tahunterbit = Integer.parseInt(input.nextLine());
+        formatbuku = input.nextLine();
+        hargapembelian = Double.parseDouble(input.nextLine());
+        minimummargin = Double.parseDouble(input.nextLine());
+        stok = Integer.parseInt(input.nextLine());
+        rating = Double.parseDouble(input.nextLine());
+        if (rating >= 4.7) {
+            kategori = "Best Pick";
+        } else {
+            if (rating >= 4.5) {
+                kategori = "Must Read";
+            } else {
+                if (rating >= 4.0) {
+                    kategori = "Recommended";
+                } else {
+                    if (rating >= 3.0) {
+                        kategori = "Average";
+                    } else {
+                        if (rating < 3.0) {
+                            kategori = "Low";
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(isbn + "|" + judul + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbuku + "|" + hargapembelian + "|" + minimummargin + "|" + stok + "|" + rating + "|" + kategori);
+    }
+}
